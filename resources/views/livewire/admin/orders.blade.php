d8fddb8f763454ef9ca9d370bb378db62eedb522
Loick Hommet <hommet.loick@gmail.com>
2026-09-24 11:22:23 +0200

    Tableau affiche info

--- resources/views/livewire/admin/orders.blade.php
+++ resources/views/livewire/admin/orders.blade.php
@@ -1,5 +1,45 @@
 <div>
     <h1>Gestion des commandes</h1>
 
-    <p>{{ $orders->count() }} commandes trouvées.</p>
+    <table>
+        <thead>
+            <tr>
+                <th>Référence</th>
+                <th>Date</th>
+                <th>Client</th>
+                <th>Nombre d'articles</th>
+                <th>Montant</th>
+                <th>Statut</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            @foreach ($orders as $order)
+                <tr>
+                    <td>{{ $order->reference }}</td>
+
+                    <td>
+                        {{ $order->created_at->format('d/m/Y') }}
+                    </td>
+
+                    <td>
+                        {{ $order->customer->firstname }}
+                        {{ $order->customer->lastname }}
+                    </td>
+
+                    <td>
+                        {{ $order->details_count }}
+                    </td>
+
+                    <td>
+                        {{ number_format($order->total_amount, 2, ',', ' ') }} €
+                    </td>
+
+                    <td>
+                        {{ $order->status }}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </div>
