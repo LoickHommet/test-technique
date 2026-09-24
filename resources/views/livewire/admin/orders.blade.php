16532a878fc56b2f1f4dfc9e4f6eeaff073426ca
Loick Hommet <hommet.loick@gmail.com>
2026-09-24 11:25:54 +0200

    Ajoue des filtre dans la table

--- resources/views/livewire/admin/orders.blade.php
+++ resources/views/livewire/admin/orders.blade.php
@@ -1,6 +1,31 @@
 <div>
     <h1>Gestion des commandes</h1>
 
+    <div>
+        <input
+            type="text"
+            wire:model="search"
+            placeholder="Référence, client ou e-mail">
+
+        <select wire:model="status">
+            <option value="">Tous les statuts</option>
+
+            @foreach (\App\Models\Order::STATUSES as $orderStatus)
+            <option value="{{ $orderStatus }}">
+                {{ ucfirst($orderStatus) }}
+            </option>
+            @endforeach
+        </select>
+
+        <input
+            type="date"
+            wire:model="dateFrom">
+
+        <input
+            type="date"
+            wire:model="dateTo">
+    </div>
+
     <table>
         <thead>
             <tr>
@@ -15,30 +40,30 @@
 
         <tbody>
             @foreach ($orders as $order)
-                <tr>
-                    <td>{{ $order->reference }}</td>
+            <tr>
+                <td>{{ $order->reference }}</td>
 
-                    <td>
-                        {{ $order->created_at->format('d/m/Y') }}
-                    </td>
+                <td>
+                    {{ $order->created_at->format('d/m/Y') }}
+                </td>
 
-                    <td>
-                        {{ $order->customer->firstname }}
-                        {{ $order->customer->lastname }}
-                    </td>
+                <td>
+                    {{ $order->customer->firstname }}
+                    {{ $order->customer->lastname }}
+                </td>
 
-                    <td>
-                        {{ $order->details_count }}
-                    </td>
+                <td>
+                    {{ $order->details_count }}
+                </td>
 
-                    <td>
-                        {{ number_format($order->total_amount, 2, ',', ' ') }} €
-                    </td>
+                <td>
+                    {{ number_format($order->total_amount, 2, ',', ' ') }} €
+                </td>
 
-                    <td>
-                        {{ $order->status }}
-                    </td>
-                </tr>
+                <td>
+                    {{ $order->status }}
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
