327429af0672f1a7bae753fa070b3f6bcf54aef5
Loick Hommet <hommet.loick@gmail.com>
2026-09-24 11:25:43 +0200

    Creation des filtre

--- app/Http/Livewire/Admin/Orders.php
+++ app/Http/Livewire/Admin/Orders.php
@@ -7,11 +7,42 @@ use Livewire\Component;
 
 class Orders extends Component
 {
+    public $search = '';
+    public $status = '';
+    public $dateFrom = '';
+    public $dateTo = '';
+
     public function render()
     {
         $orders = Order::query()
             ->with('customer')
             ->withCount('details')
+
+            ->when($this->search, function ($query) {
+                $search = '%' . $this->search . '%';
+
+                $query->where(function ($query) use ($search) {
+                    $query->where('reference', 'like', $search)
+                        ->orWhereHas('customer', function ($query) use ($search) {
+                            $query->where('firstname', 'like', $search)
+                                ->orWhere('lastname', 'like', $search)
+                                ->orWhere('email', 'like', $search);
+                        });
+                });
+            })
+
+            ->when($this->status, function ($query) {
+                $query->where('status', $this->status);
+            })
+
+            ->when($this->dateFrom, function ($query) {
+                $query->whereDate('created_at', '>=', $this->dateFrom);
+            })
+
+            ->when($this->dateTo, function ($query) {
+                $query->whereDate('created_at', '<=', $this->dateTo);
+            })
+
             ->latest()
             ->get();
 
